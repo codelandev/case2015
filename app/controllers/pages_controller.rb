13d3b8e9519fe501534991d4281e5fd08699bc3e
André Pinho <andresouzapinho@gmail.com>
2015-04-21 00:52:04 -0300

    Integrates sponsors with home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   def home
+    ## Events
     @events = Event.in_the_future.order(happens_at: :asc)
     @event_days = []
     @events.pluck(:happens_at).map { |datetime| datetime.to_date }.uniq .each do |date|
       events = Event.select{ |event| event.happens_at.to_date == date }
       @event_days << EventDay.new(events: events, date: date)
+
+    ## Sponsors
+    @sponsors = Sponsor.order("RANDOM()").first(6)
     end
   end
 end
