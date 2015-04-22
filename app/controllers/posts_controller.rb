902cac56af24c3128bc4ba979f9cc59e9ae57af3
Gabriel Carpenedo <cbgabe@gmail.com>
2015-04-22 20:23:08 -0300

    Improves header text

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.in_the_past.order(published_at: :asc).page(params[:page]).per(1)
+    @posts = Post.in_the_past.order(published_at: :asc).page(params[:page]).per(5)
   end
 
   def show
