5f5d1821f7ec8894141b58d1360c6cbddae33237
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-07-20 18:51:40 -0300

    Fixes problems with ckeditor language on production

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -5,6 +5,7 @@ Ckeditor.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require "ckeditor/orm/active_record"
+  config.assets_languages = ['pt-br']
 
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
