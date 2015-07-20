493160bbe9e946b8293ed309cbd96a89efc7e284
Gabriel Carpenedo <cbgabe@gmail.com>
2015-07-19 22:35:47 -0300

    Fixes wrong agenda page events order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -41,10 +41,10 @@ class PagesController < ApplicationController
 
   def events
     ## Events
-    @events = Event.in_the_future.order(happens_at: :asc)
+    @datetimes = Event.in_the_future.order(happens_at: :asc).pluck(:happens_at)
     @event_days = []
-    @events.pluck(:happens_at).map { |datetime| datetime.to_date }.uniq .each do |date|
-      events = Event.select{ |event| event.happens_at.to_date == date }
+    @datetimes.map { |datetime| datetime.to_date }.uniq.each do |date|
+      events = Event.order(happens_at: :asc).select{ |event| event.happens_at.to_date == date }
       @event_days << EventDay.new(events: events, date: date)
     end
   end
