c9b8d3039779fcde514e6bf9ff1420b6d97d1914
Gabriel Carpenedo <gabriel.carpenedo@gmail.com>
2015-08-24 17:38:36 -0300

    Changes blog order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.in_the_past.order(published_at: :asc).page(params[:page]).per(5)
+    @posts = Post.in_the_past.order(published_at: :desc).page(params[:page]).per(5)
   end
 
   def show
